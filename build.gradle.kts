44ff293d7a33edadd8d71051667edb2661adf86a
scaramangado <scaramangado@gmail.com>
2019-12-02 21:38:22 +0100

    Set up build script

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,2 +1,32 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+  java
+  kotlin("jvm") version "1.3.61"
+}
+
 group = "de.scaramangado"
 version = "1.0-SNAPSHOT"
+
+java {
+  sourceCompatibility = JavaVersion.VERSION_1_8
+  targetCompatibility = JavaVersion.VERSION_1_8
+}
+
+repositories {
+  mavenCentral()
+}
+
+val compileKotlin: KotlinCompile by tasks
+compileKotlin.kotlinOptions {
+  jvmTarget = "1.8"
+}
+
+val compileTestKotlin: KotlinCompile by tasks
+compileTestKotlin.kotlinOptions {
+  jvmTarget = "1.8"
+}
+
+tasks.withType<Wrapper> {
+  gradleVersion = "6.0"
+}

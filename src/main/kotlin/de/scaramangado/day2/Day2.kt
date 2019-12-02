d2ad834d7d8a4ac2d28295d1a71b1eb57df35bcd
scaramangado <scaramangado@gmail.com>
2019-12-02 23:03:24 +0100

    Answer day 2, question 2

--- src/main/kotlin/de/scaramangado/day2/Day2.kt
+++ src/main/kotlin/de/scaramangado/day2/Day2.kt
@@ -1,15 +1,38 @@
 package de.scaramangado.day2
 
 import de.scaramangado.day1.Day1
-import java.lang.IllegalStateException
+import kotlin.streams.toList
 
 fun main() {
 
-  val input = readInput().toMutableList()
+  printAnswer1()
+  println()
+  printAnswer2()
+}
+
+private fun printAnswer1() {
+  val input = originalInput.toMutableList()
   input[1] = 12
   input[2] = 2
 
+  println("Answer 1:")
   println(compute(input))
+}
+
+private fun printAnswer2() {
+
+  val matches = allPairs()
+      .parallelStream()
+      .map { it to initialState(it) }
+      .map { it.first to compute(it.second) }
+      .map { it.first to it.second[0] }
+      .filter { it.second == 19690720 }
+      .toList()
+
+  require(matches.size == 1)
+
+  println("Answer 2:")
+  println(matches[0].first.let { 100 * it.first + it.second })
 }
 
 private fun compute(intCode: List<Int>): List<Int> {
@@ -31,8 +54,31 @@ private fun compute(intCode: List<Int>): List<Int> {
   return code.toList()
 }
 
-private fun readInput(): List<Int> =
-    Day1::class.java.classLoader.getResource("day2/input_puzzle_1")?.readText()
-        ?.split(",")
-        ?.map { it.toInt() }
-        ?: emptyList()
+private fun allPairs(): List<Pair<Int, Int>> {
+
+  val allNumbers = 0..99
+  val pairs = mutableListOf<Pair<Int, Int>>()
+
+  allNumbers.forEach {
+    for (number in allNumbers) {
+      pairs.add(it to number)
+    }
+  }
+
+  return pairs
+}
+
+private fun initialState(values: Pair<Int, Int>): List<Int> {
+  val input = originalInput.toMutableList()
+  input[1] = values.first
+  input[2] = values.second
+
+  return input
+}
+
+val originalInput by lazy {
+  Day1::class.java.classLoader.getResource("day2/input_puzzle_1")?.readText()
+      ?.split(",")
+      ?.map { it.toInt() }
+      ?: emptyList()
+}

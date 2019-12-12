d0276c77c9d7dada88d64b47514ac46ce6f8b0f4
scaramangado <scaramangado@gmail.com>
2019-12-12 18:21:26 +0100

    Answer day 12, question 1

---
+++ src/main/kotlin/de/scaramangado/day12/Day12.kt
@@ -0,0 +1,161 @@
+package de.scaramangado.day12
+
+import de.scaramangado.day1.Day1
+import kotlin.math.abs
+
+fun main() {
+
+  Universe(originalInput)
+      .also { it.simulateTimeSteps(1000) }
+      .run { println("Answer 1:\n${this.getTotalEnergy()}") }
+}
+
+private class Universe(private val bodies: List<Body>) {
+
+  private val pairs = bodies.map { (bodies - it).map { list -> BodyPair(it, list) } }.flatten().toSet()
+
+  fun simulateTimeSteps(n: Int) {
+
+    require(n >= 0)
+
+    if (n == 0) return
+
+    if (n > 1) {
+      simulateTimeSteps(1)
+      simulateTimeSteps(n - 1)
+      return
+    }
+
+    simulateXStep()
+    simulateYStep()
+    simulateZStep()
+  }
+
+  fun simulateXStep() {
+    pairs.forEach {
+      with(it.body1.x - it.body2.x) {
+        when {
+          this > 0L -> {
+            it.body1.vx--
+            it.body2.vx++
+          }
+
+          this < 0L -> {
+            it.body1.vx++
+            it.body2.vx--
+          }
+
+          this == 0L -> {
+          }
+
+          else -> throw IllegalStateException()
+        }
+      }
+    }
+
+    bodies.forEach { it.x += it.vx }
+  }
+
+  fun simulateYStep() {
+    pairs.forEach {
+      with(it.body1.y - it.body2.y) {
+        when {
+          this > 0L -> {
+            it.body1.vy--
+            it.body2.vy++
+          }
+
+          this < 0L -> {
+            it.body1.vy++
+            it.body2.vy--
+          }
+
+          this == 0L -> {
+          }
+
+          else -> throw IllegalStateException()
+        }
+      }
+    }
+
+    bodies.forEach { it.y += it.vy }
+  }
+
+  fun simulateZStep() {
+    pairs.forEach {
+      with(it.body1.z - it.body2.z) {
+        when {
+          this > 0L -> {
+            it.body1.vz--
+            it.body2.vz++
+          }
+
+          this < 0L -> {
+            it.body1.vz++
+            it.body2.vz--
+          }
+
+          this == 0L -> {
+          }
+
+          else -> throw IllegalStateException()
+        }
+      }
+    }
+
+    bodies.forEach { it.z += it.vz }
+  }
+
+  fun getTotalEnergy() = bodies.map { it.getEnergy() }.sum()
+
+  override fun toString(): String = bodies.joinToString("\n") { it.toString() }
+}
+
+private class BodyPair(val body1: Body, val body2: Body) {
+  override fun equals(other: Any?): Boolean {
+    return when (other) {
+      is BodyPair -> (body1 == other.body1 && body2 == other.body2) || (body1 == other.body2 && body2 == other.body1)
+      else -> false
+    }
+  }
+
+  override fun hashCode() = body1.hashCode() + body2.hashCode()
+}
+
+private class Body(var x: Long, var y: Long, var z: Long) {
+
+  var vx = 0L
+  var vy = 0L
+  var vz = 0L
+
+  fun getEnergy() = (abs(x) + abs(y) + abs(z)) * (abs(vx) + abs(vy) + abs(vz))
+
+  override fun toString(): String = "($x, $y, $z) v($vx, $vy, $vz)"
+}
+
+private fun parsePositionalData(positionString: String): Body {
+
+  fun getCoordinate(name: String): Long {
+    return positionString.split("$name=")[1].split(Regex("[,>]"))[0].toLong()
+  }
+
+  return Body(getCoordinate("x"), getCoordinate("y"), getCoordinate("z"))
+}
+
+private val testInput by lazy {
+  """
+    <x=-8, y=-10, z=0>
+    <x=5, y=5, z=10>
+    <x=2, y=-7, z=3>
+    <x=9, y=-8, z=-3>
+  """.trimIndent()
+      .lines()
+      .map { parsePositionalData(it) }
+}
+
+private val originalInput by lazy {
+  Day1::class.java.classLoader.getResource("day12/input_puzzle_1")?.readText()
+      ?.lines()
+      ?.map { parsePositionalData(it) }
+      ?: emptyList()
+}
